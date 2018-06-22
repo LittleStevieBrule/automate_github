8c6e1fce14134577d5228a1b58c8fe41f15de301
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2018-06-22 15:30:15 -0700

    refactored login page to open at http://github.com/login

--- lib/login_page.rb
+++ lib/login_page.rb
@@ -1,11 +1,16 @@
 class LoginPage
   include PageObject
 
-  def initialize
-    super(Config.instance.driver)
+  def initialize(url = false)
+    super(Config.instance.driver, url)
   end
 
+  page_url('https://github.com/login')
+
+  text_field(:login_field, id: 'login_field')
+
   def login(username, password)
-    
+    self.login_field = username
+    binding.pry
   end
 end
